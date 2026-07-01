6a83f90841a5b566bab8e1dea8ac95170596b8e0
kodhh <89465249+kodhh@users.noreply.github.com>
2026-07-01 10:30:33 +0800

    Enable SELINUX_IGNORE_NEVERALLOWS in BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,7 +143,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.$(TARGET_BOARD_PLATFOR
 #include device/mediatek/sepolicy/sepolicy.mk
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-#SELINUX_IGNORE_NEVERALLOWS := true
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Disable dex pre-opt
 ifeq ($(HOST_OS),linux)
