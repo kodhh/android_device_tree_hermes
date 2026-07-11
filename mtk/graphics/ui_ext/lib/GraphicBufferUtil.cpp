6b3f75b707ed60dfa1b9a3ba0ceb52acd4712b8c
root <root@runnervm5mmn9.icicddbjmfvephrpzukohovmhb.bx.internal.cloudapp.net>
2026-07-11 06:21:34 +0000

    hermes: Fix SkUserConfig.h include path for skia headers

--- mtk/graphics/ui_ext/lib/GraphicBufferUtil.cpp
+++ mtk/graphics/ui_ext/lib/GraphicBufferUtil.cpp
@@ -11,17 +11,8 @@
 #include <ui/GraphicBufferMapper.h>
 
 #include <png.h>
+#include <SkColorData.h>
 //#include <transform_scanline.h>
-// RGB565 unpacking (replaces Skia's SkPacked16ToR32/G32/B32)
-static inline uint32_t SkPacked16ToR32(uint16_t c) {
-    return ((c >> 11) & 0x1F) << 3;
-}
-static inline uint32_t SkPacked16ToG32(uint16_t c) {
-    return ((c >> 5) & 0x3F) << 2;
-}
-static inline uint32_t SkPacked16ToB32(uint16_t c) {
-    return (c & 0x1F) << 3;
-}
 
 #include <GraphicBufferUtil.h>
 #include <graphics_mtk_defs.h>
